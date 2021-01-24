d20e0c39c40c7f0c0eaaa9aec2cef4ea98690e0b
moniqueking57 <65675759+moniqueking57@users.noreply.github.com>
2021-01-24 22:11:06 +0000

    Bike project done

--- exercises/F-highlight-words/script.js
+++ exercises/F-highlight-words/script.js
@@ -1,7 +1,7 @@
 function highlightWords(paragraph, colours) {
   let content = document.querySelector("#content");
   let select = document.createElement("select");
-  let pea = document.createElement("h1");
+  let pea = document.createElement("h3");
   colours.forEach(element => {
     let option = document.createElement("option");
     option.value = element;
@@ -12,7 +12,7 @@ select.addEventListener("click", event =>{
       console.log(event.target.value);
       if(event.target.value === element){
        pea.innerHTML = `${paragraph}`
-   
+        
       }
     
     })
@@ -20,8 +20,9 @@ select.addEventListener("click", event =>{
   })
 content.appendChild(pea);
   content.appendChild(select);
-     
+    
 }
+ 
 
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
@@ -30,17 +31,26 @@ const colours = ["yellow", "green", "blue", "none"];
 
 highlightWords(paragraph, colours);
 
-// let wordsArr = paragraph.split(' ');// changes the paragraph into an array
-// for(let i =0; i < wordsArr.length; i++) {
-//     let span = document.createElement('span');
-//     span.innerHTML = `${wordsArr[i]} `;
-//     span.addEventListener("click", function (e) {
-//       e.target.style.backgroundColor = selectBox.value;
-//     })
-//     pGraph.appendChild(span);
-// }
+//   // The paragraph is split to create multiple spans
+//   let words = paragraph.split(' ');
+//   words.forEach(function(word) {
+//      let span = document.createElement('span');
+//      let text3 = document.createTextNode(word + ' ');
+//      span.appendChild(text3);
+//      p.appendChild(span);
 
+//     // A callback function is created to check the value of the select option to be used as a background color to be initiated later by the event listener.
+//     let singleSpan = function () {
+//        document.getElementsByTagName('select').value;
+//        if (select.value !='none') {
+//          span.style.backgroundColor = select.value;
+//        } else if (select.value = 'none'){
+//          span.style.backgroundColor = null;
+//        }
+//     }
 
-// document.getElementById('content').appendChild(selectBox);//this adds the select box to the document
-// document.getElementById("content").appendChild(pGraph);
+//      // Refers to the callback function when a user clicks on a word.
+//      span.addEventListener('click', singleSpan);
+
+//   })
 // }
--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,11 +1,11 @@
  function buttonColors() {
-  let content = document.querySelector("#content");
   let boringButton = document.getElementById("blueBtn");
   let boringButton2 = document.getElementById("orangeBtn");
   let boringButton3 = document.getElementById("greenBtn");
   let jumboCrap = document.getElementsByClassName("jumbotron");
   let donateShit = document.getElementById("donateShit");
   let meedaxa = document.getElementById("meedaxa");
+
   boringButton.addEventListener("click", event => {
         jumboCrap[0].style.backgroundColor = "#588fbd";
         donateShit.style.backgroundColor = `#ffa500`;
@@ -19,7 +19,6 @@ boringButton2.addEventListener("click", event =>{
         meedaxa.style.backgroundColor = `#31b0d5` ;
         meedaxa.style.color = `white`;
 })
-
 boringButton3.addEventListener("click", event =>{
     jumboCrap[0].style.backgroundColor = `#87ca8a`;
         donateShit.style.backgroundColor = `black`;
@@ -28,3 +27,41 @@ boringButton3.addEventListener("click", event =>{
 
  }
  buttonColors();
+
+ //task 2
+  let subBtn = document.getElementById("entertothemagicland");
+
+ function formValidation() {
+    var formInput = document.getElementsByClassName("form-control");
+    let numberOfInvalidFields = 0;
+     for (let i= 0; i< formInput.length; i++) {
+         if (!(formInput[i].value).length > 0){
+            numberOfInvalidFields++;
+             formInput[i].style.backgroundColor = "#ffb1ad";
+         } 
+     };
+
+     let email = formInput[0].value
+     // email is just a string
+     console.log(email);
+     if (!email.includes("@")){
+         numberOfInvalidFields += 1;
+          formInput[0].style.backgroundColor = "#ffb1ad";
+     }
+
+     if(numberOfInvalidFields === 0){
+         let form = document.getElementById("topsecret");
+         form.reset();
+            alert("Thank you for filling out the form about your VPN's"); 
+     }
+ }
+
+//  event that occurs on click of submit with a prevention of refresh
+  subBtn.addEventListener("click", function(event){
+      event.preventDefault();
+      formValidation();
+  });
+
+  //random workings:
+  //  
+     // && !(formInput[0].value).includes("@")
